9e2f2ff396e22e09b1a5a160e18020ae8616c947
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2024-07-17 11:53:41 -0500

    Update jul17.py

--- src/jul17.py
+++ src/jul17.py
@@ -2,42 +2,56 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
 from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
+from pybricks.tools import wait, multitask, run_task
 
 from mylib import MyRobot
 
 bot = MyRobot()
 
-notes = ["C4/4","D4/4","E4/4","F4/4","G4/4"]
-bot.music.update(notes)
-bot.music.start()
+go_ahead = True
+play_music = True
 
-state = "go_ahead"
+music_note = ["C4/4","D4/4","E4/4","F4/4","G4/4"]
 
-def go_ahead():
-    global bot, state
-    bot.set_vel(100)
-    bot.music.tick()
-    if bot.color_sensor.color(True) == Color.WHITE:
-        bot.hub.speaker.beep()
-    if bot.force_pressed(3):
-        bot.music.stop()
-        state = "stopped"
-
-def stopped():
-    global bot, state
-    bot.stop()
-    bot.music.tick()
-    if bot.force_pressed(3):
-        bot.music.start()
-        state = "go_ahead"
+async def go_ahead():
+    global bot, go_ahead
+    while True:
+        if go_ahead:
+            bot.set_vel(100)
+        else:
+            bot.stop()
+        await wait(20)
 
 
-while True:
-    handler = {
-        "go_ahead": go_ahead,
-        "stopped": stopped
-    }
-    handler[state]()
-    wait(20)
-    
+async def read_force():
+    global bot, go_ahead
+    while True:
+        if await bot.force_pressed(3):
+            go_ahead = not go_ahead
+        await wait(20)
+
+
+async def detect_paper():
+    global bot, go_ahead, play_music
+    while True:
+        # bot.hub.display.number(await bot.color_sensor.reflection() % 100)
+        if go_ahead and await bot.color_sensor.color(True) == Color.WHITE:
+            play_music = False
+            await bot.hub.speaker.beep()
+        else:
+            play_music = True
+        await wait(20)
+
+async def play_music():
+    global bot, play_music, music_note
+    while True:
+        if play_music:
+            note = music_note.pop(0)
+            music_note.append(note)
+            await bot.hub.speaker.play_notes([note])
+        await wait(200)
+
+async def main():
+    await multitask(go_ahead(), read_force(), detect_paper(), play_music())
+
+run_task(main())
